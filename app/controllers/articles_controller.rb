1e472270ea1dff85e53920e3da9b8956affda4d0
themaleem <zizo9585@gmail.com>
2020-06-11 18:04:35 +0100

    added create and retrieve article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,27 @@
 class ArticlesController < ApplicationController
-
-def show
+  def show
     @article = Article.find(params[:id])
-end
+  end
 
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @new_article = Article.new
+  end
+
+ 
+  def create
+    @new_article =
+      Article.new(params.require(:article).permit(:title, :description))
+
+    if @new_article.save
+      flash[:notice] = 'Article was successfully created.'
+      redirect_to action: 'show', id: @new_article.id
+    else
+      # redirect_to @new_article
+      render 'new'
+    end
+  end
 end
