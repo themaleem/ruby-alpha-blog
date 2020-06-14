791cecca1cd1fb2609dd2e67c23e106e2b09f5f5
themaleem <zizo9585@gmail.com>
2020-06-14 10:17:41 +0100

    user to article relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_213559) do
+ActiveRecord::Schema.define(version: 2020_06_14_082004) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
