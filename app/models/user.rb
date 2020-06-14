66f9864f978282184b43640c0192496323480c16
themaleem <zizo9585@gmail.com>
2020-06-14 12:26:42 +0100

    added authentication system to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username,
             presence: true,
@@ -11,4 +12,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 125 },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
