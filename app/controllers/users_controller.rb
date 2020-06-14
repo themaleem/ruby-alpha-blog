36501894c06ebe0cf41d9882ac36acf84368796c
themaleem <zizo9585@gmail.com>
2020-06-14 15:14:37 +0100

    added user signup on FE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
-    def new
-        @user=User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'Account was successfully created.'
+      #   redirect_to action: 'show', id: @user.id
+    else
+      render 'new'
     end
-    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end 
 end
