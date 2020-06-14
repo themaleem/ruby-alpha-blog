bd9b6968d154c1000f26f39c0f59dcc95c4b7ae0
themaleem <zizo9585@gmail.com>
2020-06-14 14:47:46 +0100

    added user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles, only: %i[show index new create edit update destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: %i[new]
 end
