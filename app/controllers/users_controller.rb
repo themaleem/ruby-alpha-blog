c15ea9dc03cf936d1c1dc0e66d2bbec723789bd8
themaleem <zizo9585@gmail.com>
2020-06-14 19:44:21 +0100

    now displaying user article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update]
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+    @articles=@user.articles
+  end
+
   def new
     @user = User.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(user)
+        email_address = user.email.downcase
+        hash=Digest::MD5.hexdigest(email_address)
+        image_src="https://www.gravatar.com/avatar/#{hash}?s=200"
+        image_tag(image_src, alt:user.username, class:"rounded mx-auto d-block")
+    end
 end
