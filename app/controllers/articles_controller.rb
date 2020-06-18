47ca34beb02a3fd81a04da2598ba8978fd3c5725
themaleem <zizo9585@gmail.com>
2020-06-18 23:21:25 +0100

    m2m relatonship for articles to categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids:[])
   end
 
   def require_same_user
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :user # foreign key field (many articles to one user)
+  
+  has_many :article_categories # many articles to many categories
+  has_many :categories, through: :article_categories
+
   validates :title, presence: true, length: { minimum: 10, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 100 }
 end
